e7a17466cbb154ecb27c139a556112a3df82bebb
deuto <dezangelique1@gmail.com>
2017-01-02 17:54:15 +0100

    maj background-color

--- questions.js
+++ questions.js
@@ -1,1 +1,14 @@
-console.log("okkkk");
+function nextQuestion() {
+    console.log("Next");
+}
+
+$(".content").click(function() {
+    if ($(this).hasClass("reponse")) {
+        $(this).css({ "background-color": "#4CAF50" });
+    } else {
+
+        $(this).css({ "background-color": "#B71C1C" });
+        $(".reponse").css({ "background-color": "#4CAF50" });
+    }
+    setTimeout(nextQuestion, 3000);
+});
